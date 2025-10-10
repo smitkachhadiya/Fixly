43348bd5a612690b65a85c340494a5910f2b2749
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-10-10 18:42:12 +0530

    feat: Setup admin controller

---
+++ server/controllers/adminController.js
@@ -0,0 +1,11 @@
+const ServiceProvider = require('../models/ServiceProvider');
+const User = require('../models/User');
+const ServiceListing = require('../models/ServiceListing');
+const Booking = require('../models/Booking');
+const Complaint = require('../models/Complaint');
+const AdminEarnings = require('../models/AdminEarnings');
+const Payment = require('../models/Payment');
+const Review = require('../models/Review');
+const Commission = require('../models/Commission');
+const asyncHandler = require('../utils/asyncHandler');
+
