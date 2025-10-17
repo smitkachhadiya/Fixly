fbd5d62f37923f8e1ceae93029c51d42ddaae2e7
Parth <limbachiyaparthh@gmail.com>
2025-10-17 20:07:00 +0530

    add beginner test for serviceProvider.model.test.js

---
+++ server/tests/models/serviceProvider.model.test.js
@@ -0,0 +1,7 @@
+const ServiceProvider = require('../../models/ServiceProvider');
+
+describe('ServiceProvider Model', () => {
+  it('should pass a placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
