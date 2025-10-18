08997ba2fc8c8b875dc78c8e0205a535eb642885
Parth <limbachiyaparthh@gmail.com>
2025-10-18 21:09:00 +0530

    add beginner test for sendEmail.test.js

---
+++ server/tests/utils/sendEmail.test.js
@@ -0,0 +1,7 @@
+const sendEmail = require('../../utils/sendEmail');
+
+describe('Send Email Utility', () => {
+  it('should pass a placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
