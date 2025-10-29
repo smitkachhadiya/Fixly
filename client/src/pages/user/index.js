80e03619b3a546d1532f47c2eff012f8d5ad75b5
MirajShekhda <mirajshekhda41@gmail.com>
2025-10-29 17:56:15 +0530

    create index.js for common export

---
+++ client/src/pages/user/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Services } from './services';
+export { default as UserProfile } from './UserProfile';
+export { default as MyBookings } from './MyBookings';
